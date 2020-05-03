bbf38cbd137bb4e1b813e897597bfd2727f183c9
deusexmachina <slayerdogukan@gmail.com>
2020-05-04 00:53:07 +0300

    pengin

---
+++ info.py
@@ -0,0 +1,50 @@
+# TELEGRAM
+token="1208256641:AAGBZdWu3UeXzQoF2wzTfgNLtRtj34ElHwE"
+url="https://api.telegram.org/bot1208256641:AAGBZdWu3UeXzQoF2wzTfgNLtRtj34ElHwE/"
+#*****************************************************************************************
+#REDDIT
+username='mrbeard01'
+password='seMbol12'
+####################################################################################
+coffin1="https://www.habervakti.com/images/haberler/2020/04/iste_sosyal_medyada_yayilan_coffin_dance_akiminin_hikyesi_h103170_e81bb.jpg"
+coffin2="https://listelist.com/wp-content/uploads/2020/04/coffin-2.jpg"
+
+
+
+local_url='C:\\python3\\telegram-bot\\corona.png'
+
+welcome="""
+**********--WELCOME--**********
+
+Welcome to the COFFUN GROUP @{} ,you're coffun member now so you can do whatever you want in this chat but don't be rude againts other coffun members
+
+These are Our BOT COMMANDS:
+
+1-) /youtube = you can pull the video to the your chat such as /youtube before you go
+2-) /currency = you can calculate currency for example /currency TRY USD 10
+3-) /movie = you can recall your favorite film or series /movie matrix
+4-) /word = if you wanna learn a word meaning you can write /word coffun
+5-) /weather = you can learn your city weather situation
+6-) /horos = if you wonder the horos you can learn many information about horos /horos fish
+7-) /meme = it sends random meme
+8-) /gif = it sends random gif
+9-) /pandemic= it gives a lot of information pandemic if you want your country case /pandemic turkey, but if you write nothing it sends top list 
+10-) /corona = it gives pandemic situation over the chart: /corona Turkey Or /corona None=Top 10 Global
+"""
+_help="""
+These are MY COMMANDS
+1-) /youtube = you can pull the video to the coffun chat such as /youtube before you go
+2-) /currency = you can calculate currency for example: /currency TRY USD 10
+3-) /movie = you can recall your favorite film or series: /movie matrix
+4-) /word = if you wanna learn a word meaning you can write: /word coffun
+5-) /weather = you can learn your city weather situation
+6-) /horos = if you wonder the horos you can learn many information about horos: /horos fish
+7-) /meme = it sends random meme
+8-) /gif = it sends random gif
+9-) /corona = it gives pandemic situation over the chart: /corona Turkey Or /corona None=Top 10 Global
+10-) /pandemic= it gives a lot of information pandemic if you want your country case /pandemic turkey, but if you write nothing it sends top list 
+"""
+
+
+_github='https://github.com/deusexmachina99/telegram'
+
