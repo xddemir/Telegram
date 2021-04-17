a83afa9a16fb17175d312dd50aa6f3fb9627ef6f
Dogukan Demir <slayerdogukan@gmail.com>
2021-04-17 23:11:19 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 class weathers():
     def __init__(self,message):
         self.message=message
-        self.api_key="5fef4b2549be6dab08b119fdff650331"
+        self.api_key="api_key"
         self._url=f'http://api.openweathermap.org/data/2.5/weather?q={self.message}&appid={self.api_key}'
 
     def get_location(self):
